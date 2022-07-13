b35fb8dc9d5fcd29667ba54c56c9b6753948ab0e
Ekulw <ljw925@gmail.com>
2022-07-12 22:17:59 -0500

    Showcase edit

--- static/script.js
+++ static/script.js
@@ -2,16 +2,24 @@ $("head").load("templates/head.html");
 $("#nav").load("templates/template.html #nav-big, #nav-small", resize);
 $("#footer").load("templates/template.html #contact, footer");
 
+window.onload = resize;
 window.onresize = resize;
+//resize();
 
 function resize() {
     var navHeight;
-    if (document.getElementById("nav-small") == null || document.getElementById("nav-big") == null)
+    if (document.getElementById("nav-small") == null || document.getElementById("nav-big") == null) {
+        console.log("case 1");
+        console.trace();
         return;
+    }
     if (document.getElementById("nav-small").clientHeight == 0) {
         navHeight = document.getElementById("nav-big").clientHeight;
+        console.log("case 2");
     }else{
         navHeight = document.getElementById("nav-small").clientHeight;
+        console.log("case 3");
+        console.trace();
     }
     document.getElementById("content").style.marginTop = navHeight+50+"px";
 }
