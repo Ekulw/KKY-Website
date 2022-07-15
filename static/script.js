1f50626586b14e8fd1fd6484951571ed6eddaf50
Ekulw <ljw925@gmail.com>
2022-07-14 22:04:21 -0500

    FA icons, picture header

--- static/script.js
+++ static/script.js
@@ -1,25 +1,20 @@
 $("head").load("templates/head.html");
 $("#nav").load("templates/template.html #nav-big, #nav-small", resize);
+$("#picture-header").load("templates/template.html #header");
 $("#footer").load("templates/template.html #contact, footer");
 
 window.onload = resize;
 window.onresize = resize;
-//resize();
+resize();
 
 function resize() {
     var navHeight;
     if (document.getElementById("nav-small") == null || document.getElementById("nav-big") == null) {
-        console.log("case 1");
-        console.trace();
         return;
-    }
-    if (document.getElementById("nav-small").clientHeight == 0) {
+    }else if (document.getElementById("nav-small").clientHeight == 0) {
         navHeight = document.getElementById("nav-big").clientHeight;
-        console.log("case 2");
     }else{
         navHeight = document.getElementById("nav-small").clientHeight;
-        console.log("case 3");
-        console.trace();
     }
     document.getElementById("content").style.marginTop = navHeight+"px";
 }
@@ -33,3 +28,13 @@ document.onclick = function(event) {
         document.getElementById("menu-dropdown").classList.remove("w3-show");
     }
 }
+
+/*fetch('static/leadership.json', { 
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(json => getEmails(json));
+
+function getEmails(json) {
+  
+}*/
